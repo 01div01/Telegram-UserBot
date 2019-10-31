fbca8dc817a5e0ee9c72d09d8f077e61116596ae
01div01 <47557064+01div01@users.noreply.github.com>
2019-10-31 21:29:47 +0530

    Update pmpermit.py

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -19,8 +19,8 @@ from userbot.modules.dbhelper import (approval, approve, block_pm, notif_off,
 # ========================= CONSTANTS ============================
 UNAPPROVED_MSG = (
     "`Bleep blop! This is a bot. Don't fret.\n\n`"
-    "`My master hasn't approved you to PM.`"
-    "`Please wait for my master to look in, he mostly approves PMs.\n\n`"
+    "`My Master Divyanshu hasn't approved you to PM.`"
+    "`Please wait for my master to look in, to avoid spam and block.\n\n`"
     "`As far as I know, he doesn't usually approve retards though.`")
 # =================================================================
 
